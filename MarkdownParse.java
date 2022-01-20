174d8aed0f4d102054079e14036184f43e3b92a4
Saransh <saranshmalik1712@gmail.com>
2022-01-20 13:42:25 -0800

    New test file for breaking behaviour
    Stuck in an infinite while loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,3 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,8 +6,6 @@ import java.util.ArrayList;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
